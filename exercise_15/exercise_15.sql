c4676533055f0ee22472428fe44dbde24e465d60
Bradley-Bird <Bradleybird2@icloud.com>
2022-06-15 16:21:28 -0700

    test 15 passing

--- exercise_15/exercise_15.sql
+++ exercise_15/exercise_15.sql
@@ -1,1 +1,5 @@
 -- the total run time of all inventory items if they were all played back to back
+SELECT SUM(film.length) AS sum
+from
+inventory 
+LEFT JOIN film on inventory.film_id = film.film_id
